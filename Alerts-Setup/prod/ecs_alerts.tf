c8c98c5fffbb2f14a901d031002da7040454637a
Vignesh251002 <vignesh.i@meyicloud.com>
2026-09-18 16:28:17 +0530

    Update ecs_alerts.tf

--- Alerts-Setup/prod/ecs_alerts.tf
+++ Alerts-Setup/prod/ecs_alerts.tf
@@ -34,7 +34,7 @@ resource "aws_cloudwatch_metric_alarm" "ecs_high_cpu" {
   statistic           = "Average"
   period              = 300
   evaluation_periods  = 2
-  threshold           = 60
+  threshold           = 70
   comparison_operator = "GreaterThanThreshold"
   treat_missing_data  = "notBreaching"
 
